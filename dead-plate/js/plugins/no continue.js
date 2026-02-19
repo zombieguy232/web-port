8f62bed0998430a81eda39a5f4ca89f2c00d70b3
zombieguy232 <154286122+zombieguy232@users.noreply.github.com>
2026-02-19 15:13:07 -0500

    Delete dead-plate directory

--- dead-plate/js/plugins/no continue.js
+++
@@ -1,5 +0,0 @@
-Window_TitleCommand.prototype.makeCommandList = function() {
-	this.addCommand(TextManager.newGame,   'newGame');
-//	this.addCommand(TextManager.continue_, 'continue', this.isContinueEnabled());
-	this.addCommand(TextManager.options,   'options');
-};
--- dead-plate/js/plugins/window_position.js
+++
@@ -1,7 +0,0 @@
-Window_ChoiceList.prototype.updatePlacement = function() {
-var positionType = $gameMessage.choicePositionType();
-this.width = this.windowWidth();
-this.height = this.windowHeight();
-this.x = 300;
-this.y = -800;
-};
